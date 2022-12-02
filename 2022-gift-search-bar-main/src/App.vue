550fb7ea08c95e869a9aacec3c3b493ebfa895ed
Viktoria Bors-Pajuste <viktoria.pajuste@gmail.com>
2022-12-02 10:04:50 +0100

    1-December

---
+++ 2022-gift-search-bar-main/src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { ref, watch } from 'vue'
+
+const searchTerm = ref('')
+let products = ref([])
+let isLoading= ref(false)
+let timeOutId
+
+const findProducts = (searchWord) => {
+  console.log(searchWord, timeOutId)
+  products.value = []
+  isLoading.value = true
+  if(timeOutId){
+    clearTimeout(timeOutId)
+  }
+
+  timeOutId = setTimeout( async ()=> {
+    fetch('https://dummyjson.com/products/search?q='+searchWord+'&limit=5')
+    .then(res => res.json())
+    .then(data => {
+      products.value = data.products        
+      isLoading.value = false
+    })
+  }, 2000)
+}
+
+
+watch(searchTerm, newTerm => findProducts(newTerm))
+</script>
+
+<template>
+  <div class="w-full h-full flex flex-col gap-5 justify-center items-center">
+    <h1 class="text-4xl font-bold">Gift Search Bar</h1>
+    <input type="text" class="p-2 border-2 border-gray-dark" v-model="searchTerm" placeholder="Start typing..." />
+    <div v-if="!isLoading && products.length > 0">
+    <ul class="list-disc">
+      <li v-for="product in products" :key="product.id">
+        {{product.title}} - {{product.price}}$
+      </li>
+    </ul>
+    </div>
+    <div v-if="isLoading">
+      Please wait...
+    </div>
+    <div v-if="products.length == 0">
+      No products found
+    </div>
+  </div>
+</template>
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # AdventOfVue
  
+## Introduction
+[Advent Of Vue](https://adventofvue.com) is a series of 24 Vue coding challenges that are sent out every day from December 1 to December 24.
+
+## My Goal
+It's easy - to practice and practice Vue :)
+
+## "Blog" 
+Get some insight about these puzzles here. Lets start!
+
+### 1. of December - Gift search
+Needed to build a debounced search bar. Debounce good to delay functions. This is specially useful when we search. We dont want to send request at every single keydown. We want to send a request after the user pause and stop writing. Debounce can help with that.
+
+I knew I need to use setTimeOut, but I actually have not met with the concept of debounce. After search on the net I found [this article](https://www.javascripttutorial.net/javascript-dom/javascript-debounce/) to help me out.
+
+My solution is not perfect, as if the search bar is empty, the previous result is still listed... Any idea what I am missing? 
